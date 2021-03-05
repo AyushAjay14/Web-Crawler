1847f092dd5703c1610116832994feac76214c1e
AyushAjay14 <ayushajay52@gmail.com>
2021-03-05 13:28:43 +0530

    creating basic functions

--- python.py
+++ python.py
@@ -14,22 +14,22 @@ def all_links(t):                 # scrapes all the 'a' tags in the soup content
     for anchors in paras:
         get_llinks.add((anchors.get('href')))       #scrapes all the links with the 'href' tag
     return(get_llinks)
-def save_links(get_links):
+def save_links(get_links):          # saves all the links in text file
     f = open("html.txt", "a+")
     for links in get_links:
-        t = re.compile(r'/[a-zA-Z0-9./-_]')
-        if ( re.match(t, links)):
-            f.write(u + links + '\n')
+        t = re.compile(r'/[a-zA-Z0-9./-_]')                    
+        if ( re.match(t, links)):                       #it checks if the link starts with a '/' then 
+            f.write(u + links + '\n')                   # adds the url inputted by the user,at the beginning of the link
         else:
             f.write( links + "\n")
     f.close()
-def find_imgs(t):
+def find_imgs(t):                           #scrapes all the image links from the website
     get_img = set()
-    image = t.find_all('img')
+    image = t.find_all('img')               #finds all the img tags from the beautifulsoup object
     for img in image:
         get_img.add(img.get('src'))
     return(get_img)
-def save_imgs(get_imgs):
+def save_imgs(get_imgs):                    #saves all the img links found from the find_imgs function
     p = open("src.txt", "w+")
     for images in get_imgs:
         t = re.compile(r'/[a-zA-Z0-9./-_]')
@@ -46,7 +46,7 @@ def search_mails(ut):
         for i in result:
             print(i)
             e.write(i + '\n')
-def search_phone_no(ut):
+def search_phone_no(ut):                 #scrapes all the phone numbers 
     res = requests.get(ut).text
     numbers = set()
     patt = re.compile(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]')
@@ -55,17 +55,17 @@ def search_phone_no(ut):
         print(i)
         numbers.add(i)
     return (numbers)
-def save_phone_no(phn):
+def save_phone_no(phn):                   #saves all the phone numbers found in the save_phone_no function
     with open("phone_no.txt", "a+") as ph:
         for p in phn:
             ph.write(p + '\n')
 
-def take_ss(url):
+def take_ss(url):                         #function to take the screenshot 
     driver = webdriver.Chrome(executable_path='D:\chromedriver_win32\\chromedriver.exe')
     driver.get(url)
     driver.get_screenshot_as_file('wiki.png')
     driver.close()
-def find_headers(url):
+def find_headers(url):                     #scrapes all the http headers
     resp = requests.get(url)
     heads = resp.headers
     st = str(heads)
